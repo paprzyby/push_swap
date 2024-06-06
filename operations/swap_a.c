bb5677519a9a5d82f4852e190fb4dd105f0240ea
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-06-06 18:25:02 +0200

    starting swap_a

--- operations/swap_a.c
+++ operations/swap_a.c
@@ -1,27 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   swap_a.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/04 16:42:06 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/06/06 17:53:07 by paprzyby         ###   ########.fr       */
+/*   Created: 2024/06/06 15:54:19 by paprzyby          #+#    #+#             */
+/*   Updated: 2024/06/06 17:56:42 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#if !defined(PUSH_SWAP_H)
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include <unistd.h>
-
-typedef struct s_list
+void	swap_a(t_list **lst, t_list *current)
 {
-	int				*num;
-	struct s_list	*next;
-	struct s_list	*current;
-}					t_list;
-
-
-
-#endif
+	current = &lst;
+	if (current != NULL)
+	{
+		
+	}
+}
