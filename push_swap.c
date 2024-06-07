8459f3d69d427cdeab7c00b5687dd3312d29da66
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-06-07 15:14:13 +0200

    adding the a and b stack

--- push_swap.c
+++ push_swap.c
@@ -6,15 +6,20 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 16:41:38 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/06/04 17:05:08 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/06/07 15:14:09 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
 int	main(int argc, char **argv)
 {
-	if (argc > 1)
-	{
+	t_list	*a;
+	t_list	*b;
 
-	}
+	a = NULL;
+	b = NULL;
+	if (argc == 1)
+		return (1);
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 16:42:06 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/06/06 17:53:07 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/06/07 15:06:40 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@
 
 typedef struct s_list
 {
-	int				*num;
+	int				num;
 	struct s_list	*next;
 	struct s_list	*current;
+	struct s_list	*prev;
 }					t_list;
 
 
