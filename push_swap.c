506a4cd618c57586fba408341e83d133c963fa90
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-06-12 13:34:01 +0200

    added i case for one argument

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 16:41:38 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/06/12 10:35:28 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/06/12 13:31:14 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,32 @@
 
 int	main(int argc, char **argv)
 {
-	t_list	*a;
-	t_list	*b;
+	t_list			*a;
+	t_list			*b;
+	unsigned int	count;
 
 	a = NULL;
 	b = NULL;
+	count = 0;
 	if (argc == 1)
 		return (1);
 	else if (argc == 2)
 	{
 		argv = ft_split(argv[1], ' ');
+		while (argv[count] != NULL)
+			count++;
+		if (count == 1)
+		{
+			free(argv);
+			return (1);
+		}
 		stack_init(&a, argv);
 	}
 	else
 		stack_init(&a, argv + 1);
 	while (a)
 	{
-		printf("%d. node\n", a->value);
+		printf("Node = %d\n", a->value);
 		a = a->next;
 	}
 	return (0);
