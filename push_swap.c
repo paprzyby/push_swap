82a0765410de9af867dc350bc8cfcacada7c82c0
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-06-17 10:08:54 +0200

    optimizing the code

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 16:41:38 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/06/17 09:48:49 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/06/17 10:06:58 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,17 @@ int	main(int argc, char **argv)
 {
 	t_list			*a;
 	t_list			*b;
-	unsigned int	count;
 
 	a = NULL;
 	b = NULL;
-	count = 0;
 	if (argc == 1)
 		return (1);
 	else if (argc == 2)
 	{
 		argv = ft_split(argv[1], ' ');
-		while (argv[count] != NULL)
-			count++;
-		if (count == 1)
+		if (!argv[1])
 		{
-			free(argv);
+			free(argv[0]);
 			return (1);
 		}
 		stack_init(&a, argv);
